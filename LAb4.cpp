0c3bf96ce3d6da1eb1c564b9b4462236f1d085c7
Artem Samusev <samusev.a@live.unic.ac.cy>
2021-05-31 13:15:44 +0300

    add menu

--- LAb4.cpp
+++ LAb4.cpp
@@ -3,19 +3,18 @@
 //Matrix library
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void showMenu() {
+    cout << "\n1) Fill Matrix with random numbers";
+    cout << "\n2) Sum of a specific row";
+    cout << "\n3) Sum of a specific column";
+    cout << "\n4) Show matrix";
+    cout << "\n5) Make Identity Matrix";
+    cout << "\n6) Test if Identity Matrix";
+    cout << "\n7) Sum of Diagonal";
+    cout << "\n8) Sum of all rows";
+    cout << "\n9) advanced option 2";
+    cout << "\n0) Exit";
+    cout << "\nEnter choice: ";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
